c84edcf9f8570d562e7381b054929b8c37b4ce27
Dangield <dan.gield@gmail.com>
2023-03-02 12:33:48 +0100

    Moved some old files from another repo

---
+++ MK1.5/udp_cam_sender.py
@@ -0,0 +1,64 @@
+import cv2
+import socket
+import struct
+from math import ceil
+
+
+pc_ip = "192.168.69.129" # "127.0.0.1"
+
+
+class FrameSegment(object):
+    """ 
+    Object to break down image frame segment
+    if the size of image exceed maximum datagram size 
+    """
+    MAX_DGRAM = 2**16
+    MAX_IMAGE_DGRAM = MAX_DGRAM - 64 # extract 64 bytes in case UDP frame overflown
+    def __init__(self, sock, port, addr):
+        self.s = sock
+        self.port = port
+        self.addr = addr
+
+    def udp_frame(self, img):
+        """ 
+        Compress image and Break down
+        into data segments 
+        """
+        compress_img = cv2.imencode('.jpg', img)[1]
+        dat = compress_img.tostring()
+        size = len(dat)
+        count = ceil(size/(self.MAX_IMAGE_DGRAM))
+        array_pos_start = 0
+        while count:
+            array_pos_end = min(size, array_pos_start + self.MAX_IMAGE_DGRAM)
+            self.s.sendto(struct.pack("B", count) +
+                dat[array_pos_start:array_pos_end], 
+                (self.addr, self.port)
+                )
+            array_pos_start = array_pos_end
+            count -= 1
+
+
+def main():
+    """ Top level main function """
+    # Set up UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    port = 12345
+
+    fs = FrameSegment(s, port, addr=pc_ip)
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 60)
+    fps = int(cap.get(5))
+    print("fps:", fps)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    while (cap.isOpened()):
+        _, frame = cap.read()
+        fs.udp_frame(frame)
+    cap.release()
+    cv2.destroyAllWindows()
+    s.close()
+
+if __name__ == "__main__":
+    main()
